e4865964fede49abd7487691ac3d0784b26b5a00
Elena <ebe2017@yandex.ru>
2024-04-26 20:38:07 +0400

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 import './slider';
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
+
+console.log(document.querySelector('.decoration_slider'));
+console.log(document.querySelector('.no_click'));
+console.log(document.querySelector('.decoration_content > div > div'));
+console.log(document.querySelector('.after_click'));
