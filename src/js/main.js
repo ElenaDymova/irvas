1b9bd6b8e1e0cf8d2c56ac21bb1b177b2f2a88dd
Elena <ebe2017@yandex.ru>
2024-04-27 15:36:46 +0400

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
 import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    forms();
 });
 
-console.log(document.querySelector('.decoration_slider'));
-console.log(document.querySelector('.no_click'));
-console.log(document.querySelector('.decoration_content > div > div'));
-console.log(document.querySelector('.after_click'));
