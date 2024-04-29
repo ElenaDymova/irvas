3c511b86962d69691429936be490edf1ddb4ed4f
Elena <ebe2017@yandex.ru>
2024-04-29 19:47:10 +0400

    animation

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 // const dist = "./dist/";
-const dist = "/Applications/MAMP/htdocs/test";
+const dist = "/Applications/MAMP/htdist/test";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
